4064da57cfa0f36035aee5ba6d3a41843517e077
Artyom Alekseyevich <themonadopower@gmail.com>
2018-11-04 15:11:46 -0800

    some work on finding div tags

--- main.py
+++ main.py
@@ -6,6 +6,25 @@ print('\nPlease wait while the document loads... For large files this could take
 with open('message.html') as html:
     file = soup(html, 'html.parser')
 
-print('File has finished loading. Parsing data...')
+print('File has finished loading. Parsing data...\n')
 
+#Print Chat Name
 print('Performing analysis on {0}...'.format(file.title.string))
+
+print('Parsing the file...')
+
+#Create a dictionary of participants
+participants = dict()
+
+#Create a dictionary of the participant's names
+aliases = dict()
+
+print(file.body.div.contents)
+#Loop through every tag in the file
+tag = file.body
+while len(tag) > 0:
+    #print (str(tag) + ' ')
+    tag = tag.div
+    print(type(tag))
+    if (tag == None):
+        break
