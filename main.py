5cc4de223a5953022d392e26f2d1e6aec3f84aa5
Artyom Alekseyevich <themonadopower@gmail.com>
2018-11-04 16:36:52 -0800

    addition of word counts, character counts, list of most common words

--- main.py
+++ main.py
@@ -6,6 +6,8 @@ If you really want to figure it out, do the hard work I did
 
 
 from bs4 import BeautifulSoup as soup
+import re
+from collections import Counter
 
 print('Nate\'s HTML Parser - Version 0.0.1')
 print('\nPlease wait while the document loads... For large files this could take upwards of several minutes...')
@@ -45,5 +47,37 @@ messages = parser.contents[1:]
 message_count = len(messages)
 print(message_count)
 
-#for message in messages:
+#Word Count of all messages
+word_count = 0
+#Character Count of all messages
+character_count = 0
+
+
+#Counter containing all words
+word_list = Counter()
+
+
+#Iterate through all messages
+for message in messages:
+    content = message.contents
+    user = content[0].string
+    #print(user)
+    message = content[1].div.contents[1].string
     #print(message)
+    date = content[2].string
+    #print(date)
+    word_count += len(message.split())
+    character_count += len(message)
+
+    words = re.findall(r'\w+', message)
+    #print(words)
+    words = [word.title() for word in words]
+    #print(words)
+    word_list.update(words)
+    #print('{0} on {1} said: "{2}"'.format(user,date,message))
+
+print(word_count)
+print(character_count)
+
+#TODO: Ignore common english words
+print(word_list.most_common(50))
