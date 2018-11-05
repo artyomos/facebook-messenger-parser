19460381f738ca786c0399543db4b4a9bee5fe60
Nate Ruppert <nate.ruppert@csu.fullerton.edu>
2018-11-04 23:25:14 -0800

    final runthrough of file, removing unnecessary elements

--- main.py
+++ main.py
@@ -9,6 +9,7 @@ from bs4 import BeautifulSoup as soup
 import re
 from collections import Counter
 
+
 def main(messenger_chat):
     print('Nate\'s Messenger (Facebook) Chat Parser - Version 1.0.0')
 
@@ -17,46 +18,43 @@ def main(messenger_chat):
         with open('message.html') as html:
             file = soup(html, 'html.parser')
         print('File has finished loading. Parsing data...\n')
-    except:
-        print('You give me a message.html to work with!')
+    except FileNotFoundError:
+        print('Error! File not found. You must give me a message.html file to work with from Facebook Messenger!')
+        return None
 
-    print('Parsing the file...')
-
-    #Get the current title
+    # Get the current title
     currentTitle = file.title.string
 
-    #Print Chat Name
+    # Tell the user analysis has begun
     print('Performing analysis on {0}...'.format(currentTitle))
 
-    #navigate to where messenger arranges the messages
+    # Navigate to where messenger stores messages
     parser = file.body.div.div.div.contents[1].contents[1]
 
-    #Organizes the participants in their respective dictionary keys
+    # Organizes the participants
     parse_participants(parser.div.div.string)
 
-    #Get the messages
+    # Get the messages and sets the chat's total messages
     messages = parser.contents[1:]
-
-    #Sets the chat's total messages
     messenger_chat['total_messages'] = len(messages)
 
+    print('\nParsing Chat Messages...')
 
-
-    #Iterate through all messages
+    # Iterate through all messages and parse data
     for message in messages:
         content = message.contents
 
         payload = {
-            'user':content[0].string,
-            'message':content[1].div.contents[1].string,
-            'date':content[2].string,
+            'user': content[0].string,
+            'message': content[1].div.contents[1].string,
+            'date': content[2].string,
         }
 
-        #Increase User's total message count
+        # Increase User's total message count
         messenger_chat['members'][payload['user']]['total_messages'] += 1
-        #Parse through the words
+        # Parse through the words
         if not parse_words(payload):
-            # If the message is not Null, fix the mesage with no linebreak statements
+            # If the message is not None/empty, fix the mesage with no linebreak (</br>) statements
             if not content[1].div.contents[1]:
                 message = ''
                 for item in content[1].div.contents[1].contents:
@@ -65,114 +63,99 @@ def main(messenger_chat):
                 payload['message'] = message
                 if not parse_words(payload):
                     print("A Secondary Parse Failed... :(")
-        #Parse through the reactions
+        # Parse through the reactions
         parse_reactions(payload, message)
-        #Parse through for images and videos
-        parse_media(payload,message)
-        #Find and parse links send in the chat
-        parse_links(payload,message)
+        # Parse through for images and videos
+        parse_media(payload, message)
+        # Find and parse links send in the chat
+        parse_links(payload, message)
 
-    print("Analysis was successful!")
-    print('Writing to file anaylsis results...')
+    print("Done! Analysis was successful!")
+    print('\nWriting to file anaylsis results...')
 
-
-    #Give a usable list in console and write to file
+    # Give a usable list in console and write to file
     with open('messenger_stats.txt', 'w') as f:
-        #Header
+        # Header
         f.write('File Generated Using Nate\'s Messenger Parser (https://github.com/artyomos/messenger-parser)\nVersion 0.0.1\n\n')
 
         # Group Chat Title
         f.write('Messenger Chat: {0}\n\n'.format(currentTitle))
 
-        #Stats
-        f.write('Total Messages: {0}\n'.format(messenger_chat['total_messages']))
+        # Stats
+        f.write('Total Messages: {0}\n'.format(
+            messenger_chat['total_messages']))
         f.write('Word Count: {0}\n'.format(messenger_chat['word_count']))
-        f.write('Character Count: {0}\n'.format(messenger_chat['character_count']))
+        f.write('Character Count: {0}\n'.format(
+            messenger_chat['character_count']))
         f.write('Images Sent: {0}\n'.format(messenger_chat['image_count']))
         f.write('Gifs Sent: {0}\n'.format(messenger_chat['gif_count']))
         f.write('Videos Sent: {0}\n'.format(messenger_chat['video_count']))
-        f.write('Audio Files Sent: {0}\n'.format(messenger_chat['audio_count']))
+        f.write('Audio Files Sent: {0}\n'.format(
+            messenger_chat['audio_count']))
         f.write('Links Sent: {0}\n'.format(messenger_chat['link_count']))
-        f.write('Reactions Given: {0}\n'.format(messenger_chat['reaction_count']['given']))
+        f.write('Reactions Given: {0}\n'.format(
+            messenger_chat['reaction_count']['given']))
         for reaction in messenger_chat['reaction_count']['reaction_counter']:
-            f.write('\t{0}:{1}'.format(reaction, messenger_chat['reaction_count']['reaction_counter'][reaction]))
-        #TODO Add other section
+            f.write('\t{0}:{1}'.format(
+                reaction, messenger_chat['reaction_count']['reaction_counter'][reaction]))
+        # TODO Add other section
         f.write('\nThe 50 Most Common Words:\n')
         words = remove_common(messenger_chat['words_counter']).most_common(50)
-        for num in range (len(words)):
-            f.write('\t{0}. {1} ({2}x)'.format(num+1, words[num][0], words[num][1]))
+        for num in range(len(words)):
+            f.write('\t{0}. {1} ({2}x)'.format(
+                num + 1, words[num][0], words[num][1]))
 
-        #User Stats
+        # User Stats
         f.write('\n\nStats by Member:')
         for user in messenger_chat['members']:
             individual = messenger_chat['members'][user]
             f.write('\n\n{0}\n\n'.format(user))
-            #Stats per user
-            f.write('Total Messages: {0}\n'.format(individual['total_messages']))
+            f.write('Total Messages: {0}\n'.format(
+                individual['total_messages']))
             f.write('Word Count: {0}\n'.format(individual['word_count']))
-            f.write('Character Count: {0}\n'.format(individual['character_count']))
+            f.write('Character Count: {0}\n'.format(
+                individual['character_count']))
             f.write('Images Sent: {0}\n'.format(individual['image_count']))
             f.write('Gifs Sent: {0}\n'.format(individual['gif_count']))
             f.write('Videos Sent: {0}\n'.format(individual['video_count']))
-            f.write('Audio Files Sent: {0}\n'.format(individual['audio_count']))
+            f.write('Audio Files Sent: {0}\n'.format(
+                individual['audio_count']))
             f.write('Links Sent: {0}\n'.format(individual['link_count']))
-            f.write('Reactions Given: {0}\n'.format(individual['reaction_count']['given']))
+            f.write('Reactions Given: {0}\n'.format(
+                individual['reaction_count']['given']))
             for reaction in individual['reaction_count']['given_counter']:
-                f.write('\t{0}:{1}'.format(reaction, individual['reaction_count']['given_counter'][reaction]))
-            f.write('\nReactions Received: {0}\n'.format(individual['reaction_count']['received']))
+                f.write('\t{0}:{1}'.format(
+                    reaction, individual['reaction_count']['given_counter'][reaction]))
+            f.write('\nReactions Received: {0}\n'.format(
+                individual['reaction_count']['received']))
             for reaction in individual['reaction_count']['received_counter']:
-                f.write('\t{0}:{1}'.format(reaction, individual['reaction_count']['received_counter'][reaction]))
-            #TODO Add other section
+                f.write('\t{0}:{1}'.format(
+                    reaction, individual['reaction_count']['received_counter'][reaction]))
+            # TODO Add other section
             f.write('\nThe 25 Most Common Words:\n')
             words = remove_common(individual['words_counter']).most_common(25)
-            for num in range (len(words)):
-                f.write('\t{0}. {1} ({2}x)'.format(num+1, words[num][0], words[num][1]))
+            for num in range(len(words)):
+                f.write('\t{0}. {1} ({2}x)'.format(
+                    num + 1, words[num][0], words[num][1]))
+    print('Wrote Results to messenger_stats.txt. Please check that file for details!\n\nThanks for using my program :)!')
 
 
 def remove_common(counter):
-    #Common useless messenger words
-    common_words = ['A','An','The','I','To','And','That','Sent','Is','Photo','S','T','You','Of','Like', 'It', 'In','My','This','For','M','We','At','Was','On','So','But','Just','Be','Good','If','Ll','Attachment']
+    # Common useless messenger words
+    common_words = ['A', 'An', 'The', 'I', 'To', 'And', 'That', 'Sent', 'Is', 'Photo', 'S', 'T', 'You', 'Of', 'Like', 'It',
+                    'In', 'My', 'This', 'For', 'M', 'We', 'At', 'Was', 'On', 'So', 'But', 'Just', 'Be', 'Good', 'If', 'Ll', 'Attachment']
     for word in common_words:
         del counter[word]
     return counter
-#Dictionary containing everything about the chat
-messenger_chat = {
-    'total_messages':0,
-    'word_count':0,
-    'character_count':0,
-    'image_count':0,
-    'gif_count':0,
-    'video_count':0,
-    'audio_count':0,
-    'link_count':0,
-    'reaction_count':{
-        'given':0,
-        'reaction_counter':Counter(),
-        },
-    'other': {
-        'like_usage':0,
-        'emoji_usage':0,
-        'emoji_counter':Counter(),
-        'sticker_usage':0,
-        'sticker_counter':Counter(),
-    },
-    'words_counter':Counter(),
-    'members':{},
-}
-
 
 
 def parse_words(payload):
     try:
         words = re.findall(r'\w+', payload['message'])
         words = [word.title() for word in words]
-
-        #Update Chat Values
         messenger_chat['words_counter'].update(words)
         messenger_chat['word_count'] += len(words)
         messenger_chat['character_count'] += len(''.join(words))
-
-        #Update User's Values
         user = messenger_chat['members'][payload['user']]
         user['words_counter'].update(words)
         user['word_count'] += len(words)
@@ -187,46 +170,37 @@ def parse_words(payload):
         return False
     return True
 
+
 def parse_reactions(payload, message):
     reactions = message.find_all('li')
     if reactions:
         pure_reactions = [obj.string[:1] for obj in reactions]
-        #print(pure_reactions)
-
-        #Update Chat Values
         messenger_chat['reaction_count']['given'] += len(reactions)
-        messenger_chat['reaction_count']['reaction_counter'].update(pure_reactions)
-
-        #Update User Values
+        messenger_chat['reaction_count']['reaction_counter'].update(
+            pure_reactions)
         receiving_user = messenger_chat['members'][payload['user']]
         giving_users = [obj.string[1:] for obj in reactions]
-
-        #Receiving User
         receiving_user['reaction_count']['received'] += len(reactions)
-        receiving_user['reaction_count']['received_counter'].update(pure_reactions)
+        receiving_user['reaction_count']['received_counter'].update(
+            pure_reactions)
 
-        #Giving Users
-        #TODO Rewrite for loop
-        index = 0 #Sad but necessary
-        for user in giving_users:
+        for index, user in enumerate(giving_users):
             individual = messenger_chat['members'][user]
             individual['reaction_count']['given'] += 1
-            individual['reaction_count']['given_counter'].update(pure_reactions[index])
-            index += 1
+            individual['reaction_count']['given_counter'].update(
+                pure_reactions[index])
 
-        #print(giving_users)
 
 def parse_media(payload, message):
     user = messenger_chat['members'][payload['user']]
     images = message.find_all('img')
     if images:
         for image in images:
-            #If image is in fact a gif
+            # If image is a gif
             if image['src'][-3:] == 'gif':
                 messenger_chat['gif_count'] += 1
                 user['gif_count'] += 1
             else:
-                #Otherwise update normally one at a time
                 messenger_chat['image_count'] += 1
                 user['image_count'] += 1
     videos = message.find_all('video')
@@ -238,6 +212,7 @@ def parse_media(payload, message):
         messenger_chat['audio_count'] += len(audio)
         user['audio_count'] += len(audio)
 
+
 def parse_links(payload, message):
     user = messenger_chat['members'][payload['user']]
     links = message.find_all('a')
@@ -247,38 +222,64 @@ def parse_links(payload, message):
                 messenger_chat['link_count'] += 1
                 user['link_count'] += 1
 
+
 def parse_participants(members):
-    """ Given a String as dictated by Facebook Messenger, parse it into the messenger chat dictionary """
+    # Remove useless messenger strings
     members = members.replace('Participants:', '')
     members = members.replace(' and ', ', ')
     members = members.strip().split(', ')
-    #print(members)
+    # Add Member to members
     for participant in members:
         messenger_chat['members'][participant] = {
-            'total_messages':0,
-            'word_count':0,
-            'character_count':0,
-            'image_count':0,
-            'gif_count':0,
-            'video_count':0,
-            'audio_count':0,
-            'link_count':0,
-            'reaction_count':{
-                'given':0,
-                'received':0,
-                'given_counter':Counter(),
-                'received_counter':Counter(),
-                },
-            'other': {
-                'like_usage':0,
-                'emoji_usage':0,
-                'emoji_counter':Counter(),
-                'sticker_usage':0,
-                'sticker_counter':Counter(),
+            'total_messages': 0,
+            'word_count': 0,
+            'character_count': 0,
+            'image_count': 0,
+            'gif_count': 0,
+            'video_count': 0,
+            'audio_count': 0,
+            'link_count': 0,
+            'reaction_count': {
+                'given': 0,
+                'received': 0,
+                'given_counter': Counter(),
+                'received_counter': Counter(),
             },
-            'words_counter':Counter(),
+            'other': {
+                'like_usage': 0,
+                'emoji_usage': 0,
+                'emoji_counter': Counter(),
+                'sticker_usage': 0,
+                'sticker_counter': Counter(),
+            },
+            'words_counter': Counter(),
         }
 
-#Runs the file
+
+# Dictionary containing everything about the chat
+messenger_chat = {
+    'total_messages': 0,
+    'word_count': 0,
+    'character_count': 0,
+    'image_count': 0,
+    'gif_count': 0,
+    'video_count': 0,
+    'audio_count': 0,
+    'link_count': 0,
+    'reaction_count': {
+        'given': 0,
+        'reaction_counter': Counter(),
+    },
+    'other': {
+        'like_usage': 0,
+        'emoji_usage': 0,
+        'emoji_counter': Counter(),
+        'sticker_usage': 0,
+        'sticker_counter': Counter(),
+    },
+    'words_counter': Counter(),
+    'members': {},
+}
+
+# Runs the file
 main(messenger_chat)
-print(messenger_chat)
