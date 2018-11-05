88772bb3ada238d8daa694c2f7a7022b09707daa
Artyom Alekseyevich <themonadopower@gmail.com>
2018-11-04 17:29:10 -0800

    beginning of function encapsulation

--- main.py
+++ main.py
@@ -9,100 +9,105 @@ from bs4 import BeautifulSoup as soup
 import re
 from collections import Counter
 
-print('Nate\'s HTML Parser - Version 0.0.1')
-print('\nPlease wait while the document loads... For large files this could take upwards of several minutes...')
+def main():
+    print('Nate\'s HTML Parser - Version 0.0.1')
+    print('\nPlease wait while the document loads... For large files this could take upwards of several minutes...')
 
-with open('message.html') as html:
-    file = soup(html, 'html.parser')
+    with open('message.html') as html:
+        file = soup(html, 'html.parser')
 
-print('File has finished loading. Parsing data...\n')
+    print('File has finished loading. Parsing data...\n')
 
-#Print Chat Name
-print('Performing analysis on {0}...'.format(file.title.string))
+    #Print Chat Name
+    print('Performing analysis on {0}...'.format(file.title.string))
 
-print('Parsing the file...')
+    print('Parsing the file...')
 
-#Create a dictionary of participants
-participants = dict()
+    #Create a dictionary of participants
+    participants = dict()
 
-#Create a dictionary of the participant's names
-aliases = dict()
+    #Create a dictionary of the participant's names
+    aliases = dict()
 
-#print(file.body.div)
-#Loop through every tag in the file
+    #print(file.body.div)
+    #Loop through every tag in the file
 
-#navigate to where messenger arranges the messages
-parser = file.body.div.div.div.contents[1].contents[1]
-#print(parser)
-#Get the current title
-currentTitle = file.title.string
-print(currentTitle)
+    #navigate to where messenger arranges the messages
+    parser = file.body.div.div.div.contents[1].contents[1]
+    #print(parser)
+    #Get the current title
+    currentTitle = file.title.string
+    print(currentTitle)
 
-#TODO organize into dictionary
-participants = parser.div.div.string
-print(participants)
+    #TODO organize into dictionary
+    participants = parser.div.div.string
+    print(participants)
 
 
-messages = parser.contents[1:]
-message_count = len(messages)
-print(message_count)
+    messages = parser.contents[1:]
+    message_count = len(messages)
+    print(message_count)
 
-#Word Count of all messages
-word_count = 0
-#Character Count of all messages
-character_count = 0
+    #Word Count of all messages
+    word_count = 0
+    #Character Count of all messages
+    character_count = 0
 
+
+
+
+
+    #Iterate through all messages
+    for message in messages:
+        content = message.contents
+        user = content[0].string
+        #print(user)
+        message = content[1].div.contents[1].string
+        #print(message)
+        date = content[2].string
+        #print(date)
+        try:
+            word_count += len(message.split())
+            character_count += len(message)
+
+            words = re.findall(r'\w+', message)
+            #print(words)
+            words = [word.title() for word in words]
+
+            word_list.update(words)
+        except:
+            # If the message is not Null, fix the mesage with no linebreak statements
+            if (len(content[1].div.contents[1]) != 0):
+                message = ''
+                for item in content[1].div.contents[1].contents:
+                    if isinstance(item, str):
+                        message += item
+                parseWords(message, date)
+
+
+
+    print(word_count)
+    print(character_count)
+
+    #TODO: Ignore common english words
+    print(word_list.most_common(50))
 
 #Counter containing all words
 word_list = Counter()
 
-
-#Iterate through all messages
-for message in messages:
-    content = message.contents
-    user = content[0].string
-    #print(user)
-    message = content[1].div.contents[1].string
-    #print(message)
-    date = content[2].string
-    #print(date)
+def parseWords(message, date):
     try:
-        word_count += len(message.split())
-        character_count += len(message)
-
         words = re.findall(r'\w+', message)
-        #print(words)
         words = [word.title() for word in words]
-
         word_list.update(words)
-    except:
-        #print('{0} on {1} said: "{2}"'.format(user,date,message)
-        if (len(content[1].div.contents[1]) == 0):
-            pass
-        else:
-            print('Error: TimeStamp of {0}'.format(date))
-            #print(len(content[1].div.contents[1]))
-            #print(content[1].div.contents[1].contents)
-            message = ''
-            for item in content[1].div.contents[1].contents:
-                if isinstance(item, str):
-                    message += item
-            #print(type(content[1].div.contents[1].contents))
-            print(message)
-            try:
-                words = re.findall(r'\w+', message)
-                #print(words)
-                words = [word.title() for word in words]
-
-                word_list.update(words)
-            except TypeError:
-                print(message)
+    except TypeError:
+        print('Error: TimeStamp of {0}'.format(date))
+        print('Message: "{0}"'.format(message))
+        return False
+    return True
 
 
 
 
-print(word_count)
-print(character_count)
-
-#TODO: Ignore common english words
-print(word_list.most_common(50))
+#Runs the file
+main()
