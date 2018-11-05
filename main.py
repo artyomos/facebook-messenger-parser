94d3a8142fe46fa31a1cf1c63258e1d511e409f7
Artyom Alekseyevich <themonadopower@gmail.com>
2018-11-04 20:08:59 -0800

    consolidation of everything into a singular dictionary

--- main.py
+++ main.py
@@ -18,78 +18,53 @@ def main(messenger_chat):
 
     print('File has finished loading. Parsing data...\n')
 
-    #Print Chat Name
-    print('Performing analysis on {0}...'.format(file.title.string))
-
     print('Parsing the file...')
+
+    #Get the current title
+    currentTitle = file.title.string
+
+    #Print Chat Name
+    print('Performing analysis on {0}...'.format(currentTitle))
 
     #navigate to where messenger arranges the messages
     parser = file.body.div.div.div.contents[1].contents[1]
 
-    #Get the current title
-    currentTitle = file.title.string
-    print(currentTitle)
-
     #Organizes the participants in their respective dictionary keys
-    participants = parser.div.div.string
-    parse_participants(participants)
+    parse_participants(parser.div.div.string)
 
-
+    #Get the messages
     messages = parser.contents[1:]
-    message_count = len(messages)
-    print(message_count)
 
-    #Word Count of all messages
-    word_count = 0
-    #Character Count of all messages
-    character_count = 0
-
-
-
-
+    #Sets the chat's total messages
+    messenger_chat['total_messages'] = len(messages)
 
     #Iterate through all messages
     for message in messages:
         content = message.contents
-        user = content[0].string
-        #print(user)
-        message = content[1].div.contents[1].string
-        #print(message)
-        date = content[2].string
-        #print(date)
-        if not parse_words(message, date):
+        #TODO add everything else (Images/stickers/emoji checking)
+        payload = {
+            'user':content[0].string,
+            'message':content[1].div.contents[1].string,
+            'date':content[2].string,
+        }
+        if not parse_words(payload):
             # If the message is not Null, fix the mesage with no linebreak statements
             if (len(content[1].div.contents[1]) != 0):
                 message = ''
                 for item in content[1].div.contents[1].contents:
                     if isinstance(item, str):
                         message += item
-                if not parse_words(message, date):
-                    print("Failed :(")
-
-
-
-    print(word_count)
-    print(character_count)
+                payload['message'] = message
+                if not parse_words(payload):
+                    print("A Secondary Parse Failed... :(")
 
     #TODO: Ignore common english words
-    print(messenger_chat['chat_words'].most_common(50))
-
-
-#Create a dictionary of participants
-chat_participants = dict()
+    print(messenger_chat['words_counter'].most_common(50))
 
 #Dictionary containing everything about the chat
 messenger_chat = {
     'members':{},
-    'chat_word_count':0,
-    'chat_character_count':0,
-    'chat_reaction_count':0,
-    'chat_image_count'
-    'chat_counter':Counter(),
-}
-
-template = {
+    'total_messages':0,
     'word_count':0,
     'character_count':0,
     'image_count':0,
@@ -98,7 +73,6 @@ template = {
     'link_count':0,
     'reaction_count':{
         'given':0,
-        'received':0,
         'reaction_counter':Counter(),
         },
     'other': {
@@ -110,32 +84,51 @@ template = {
     },
     'words_counter':Counter(),
 }
-#TODO change to dictionary payload
-def parse_words(message, date):
+
+
+
+def parse_words(payload):
     try:
-        words = re.findall(r'\w+', message)
+        words = re.findall(r'\w+', payload['message'])
         words = [word.title() for word in words]
-        messenger_chat['chat_words'].update(words)
+        messenger_chat['words_counter'].update(words)
     except TypeError:
-        if message is not None:
-            print('Error: TimeStamp of {0}'.format(date))
-            print('Message: "{0}"'.format(message))
+        if payload['message'] is not None:
+            print('Error: TimeStamp of {0}'.format(payload['date']))
+            print('Message: "{0}"'.format(payload['message']))
         return False
     return True
 
 def parse_participants(members):
-    #Very clever variable names I know
+    """ Given a String as dictated by Facebook Messenger, parse it into the messenger chat dictionary """
     members = members.replace('Participants:', '')
     members = members.replace(' and ', ', ')
     members = members.strip().split(', ')
     #print(members)
     for participant in members:
         messenger_chat['members'][participant] = {
-        'word_count':0,
-        'character_count':0,
-        'aliases':[],
+            'word_count':0,
+            'character_count':0,
+            'image_count':0,
+            'gif_count':0,
+            'video_count':0,
+            'link_count':0,
+            'reaction_count':{
+                'given':0,
+                'received':0,
+                'reaction_counter':Counter(),
+                },
+            'other': {
+                'like_usage':0,
+                'emoji_usage':0,
+                'emoji_counter':Counter(),
+                'sticker_usage':0,
+                'sticker_counter':Counter(),
+            },
+            'words_counter':Counter(),
         }
-    print(messenger_chat)
 
 #Runs the file
 main(messenger_chat)
+
+print(messenger_chat)
